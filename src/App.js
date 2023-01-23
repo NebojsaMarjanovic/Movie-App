afd70c21feb6bbf3257a48f5efcf8d9ce78a68d2
nebojsa.marjanovic <nebojsa.marjanovic@teletrader.com>
2023-01-23 23:07:35 +0100

    Added movie app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
+import MovieCard from './MovieCard';
 import './App.css';
+//4181496f
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL='http://www.omdbapi.com?apikey=4181496f';
+
+
+const App = () => {
+    const [movies,setMovies]=useState([]);
+    const [searchTerm,setSearchTerm] = useState('');
+    
+    const searchMovies = async (tittle) => {
+        const response = await fetch(`${API_URL}&s=${tittle}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+
+        console.log(tittle);
+    }
+    useEffect(()=>{
+        searchMovies('Spiderman');
+    },[]);
+
+    const movie1={
+            "Title": "Spiderman in Cannes",
+            "Year": "2016",
+            "imdbID": "tt5978586",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg"
+        }
+
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+            <div className = "search">
+                <input 
+                    placeholder='Search for movies'
+                    value={searchTerm}
+                    onChange = {(e)=>setSearchTerm(e.target.value)}
+                />
+               <button onClick={()=>searchMovies({searchTerm})}>
+                Search
+               </button>
+            </div>
+
+            {
+                movies?.length>0
+                ? (
+                    <div className="container">
+                        {movies.map((movie)=>(
+                            <MovieCard movie={movie}/>
+                        ))}
+                  </div>
+                ) : (
+                    <div className='empty'>
+                        <h2>No movies found</h2>
+                    </div>
+                )
+                
+            }
+
+         
+        </div>
+    );
 }
-
+    
 export default App;
